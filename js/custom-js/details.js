99b09254fa8639c984f597ee82240793128b4ba3
Ashwin Jain <ashwjai@amazon.com>
2020-08-29 19:03:50 +0530

    Fixed radius of pointer. Added select action on buttons.

---
+++ js/custom-js/details.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var scene = document.querySelector('a-scene');
+    var splash = document.querySelector('#splash');
+    scene.addEventListener('loaded', function (e) {
+        splash.style.display = 'none';
+    });
+});
+
+$( document ).ready(function() {
+    setupClickEvents();
+});
+
+function setupClickEvents(){
+    AFRAME.registerComponent('details-room', {
+        schema: {},
+        init: function () {
+            var fuse = document.querySelector('#fuse-cursor');
+            var fuseProgress = document.querySelector('#fuse-progress');
+            var camera = document.querySelector('#main-camera-wrapper');
+            fuse.addEventListener('fusing', function (e) {
+                fuseProgress.emit('fusing');
+            });
+
+            var addToCart = document.querySelectorAll('.add-to-cart')[0];
+            addToCart.addEventListener('click', function(e) {   
+                
+                addToCart.setAttribute('visible', false);
+            });
+            var buyNow = document.querySelectorAll('.buy-now')[0];
+            buyNow.addEventListener('click', function(e) {                    
+                buyNow.setAttribute('visible', false);
+            })
+        }
+      });
+}
