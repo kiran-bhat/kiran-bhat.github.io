5dd1a15cf7c1b4740a5f7a5d9ce3c0669d79793a
Ashwin Jain <ashwjai@amazon.com>
2020-08-29 11:19:38 +0530

    Added hover action and movement with VR.

--- js/custom-js/all.js
+++ js/custom-js/all.js
@@ -1,9 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var scene = document.querySelector('a-scene');
+    var splash = document.querySelector('#splash');
+    scene.addEventListener('loaded', function (e) {
+        splash.style.display = 'none';
+    });
+});
+
 $( document ).ready(function() {
     setupCameraControls();
 });
 
 function setupCameraControls(){
-    $('#showroom').on('keypress', function(event){
-        console.log(event.which);
-    });
+    AFRAME.registerComponent('shopping-room', {
+        schema: {},
+        init: function () {
+            var fuse = document.querySelector('#fuse-cursor');
+            var fuseProgress = document.querySelector('#fuse-progress');
+            var camera = document.querySelector('#main-camera-wrapper');
+            var checkpoints = document.querySelectorAll('.checkpoint');
+            fuse.addEventListener('fusing', function (e) {
+                fuseProgress.emit('fusing');
+            });
+          
+            checkpoints.forEach(function(checkpoint) {
+                // image.setAttribute('look-at', '[camera]');
+                checkpoint.addEventListener('click', function(e) {                    
+                    var position = checkpoint.getAttribute('position')
+                    camera.setAttribute('position', {x: position.x, y: camera.getAttribute('position').y, z: position.z}    )
+                    // to: {x: position.x, y: position.y, z: camera.getAttribute('position').z}
+                    // camera.emit('imageClicked')
+                })
+            })
+
+            // boxEl.addEventListener('mouseenter', function () {
+            //     boxEl.setAttribute('scale', {x: 2, y: 2, z: 2});
+            //   });
+        }
+      });
 }
