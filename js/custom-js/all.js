9b2960fb7813f2ea3ad8d6471aeddc902c444e3e
Kiran <kirkiran@amazon.com>
2021-09-09 15:48:33 +0530

    js refactor

--- js/custom-js/all.js
+++ js/custom-js/all.js
@@ -7,7 +7,12 @@ document.addEventListener('DOMContentLoaded', function() {
         setupCameraControls();
     });
 
-        
+    var payButtons = document.querySelectorAll('.pay-button');
+    payButtons.forEach(function(payButton) {
+        payButton.addEventListener('click', function(e) {                    
+            window.location = 'https://www.amazon.in/';
+        })
+    });    
 });
 
 function setupCameraControls(){
@@ -18,30 +23,5 @@ function setupCameraControls(){
 
     fuse.addEventListener('fusing', function (e) {
         fuseProgress.emit('fusing');
-    });
-    
-    checkpoints.forEach(function(checkpoint) {
-        // image.setAttribute('look-at', '[camera]');
-        checkpoint.addEventListener('click', function(e) {                    
-            var position = checkpoint.getAttribute('position')
-            camera.setAttribute('position', {x: position.x, y: camera.getAttribute('position').y, z: position.z}    )
-            // to: {x: position.x, y: position.y, z: camera.getAttribute('position').z}
-            // camera.emit('imageClicked')
-        })
-    });
-
-    var asins = document.querySelectorAll('.asin');
-    
-    asins.forEach(function(asin) {
-        asin.addEventListener('click', function(e) {                    
-            window.location = '/details.html'
-        })
-    });
-
-    var payButtons = document.querySelectorAll('.pay-button');
-    payButtons.forEach(function(payButton) {
-        payButton.addEventListener('click', function(e) {                    
-            window.location = 'https://www.amazon.in/';
-        })
     });
 }
